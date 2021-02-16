cd8b9e757d6358f667e9dbe912eb50c27a7e96a3
Leonardo Casagrande <leoc.casagrande@hotmail.com>
2021-02-16 19:24:49 -0300

    Aula 215

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,42 +1,51 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom'
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom'
 
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost'
+//import NewPost from './NewPost/NewPost';
 import './Blog.css';
+import asyncComponent from '../../hoc/asyncComponent/asyncComponent'
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
 
-
+    state = {
+        auth: true
+    }
     render() {
-
-
         return (
             <div className="Blog">
                 <header>
                     <nav>
                         <ul>
                             <li><NavLink
-                             exact 
-                             to="/"
-                             activeClassName="my-active"
-                             activeStyle={{
-                                 color: '#fa923f',
-                                 textDecoration: 'underline'
-                             }}>Home</NavLink></li>
+                                exact
+                                to="/posts"
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Home</NavLink></li>
                             <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: '#submit',
                                 search: '?quick-submit=true'
-                                }}>New</NavLink></li>
+                            }}>New</NavLink></li>
                         </ul>
                     </nav>
                 </header>
                 {/* <Route path="/"
                     exact
                     render={() => <h1>Home</h1>} /> */}
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" exact component={NewPost} />
+                <Switch>
+                    {this.state.auth ? <Route path="/new-post" exact component={AsyncNewPost} /> : null}
+                    <Route path="/posts" component={Posts} />
+                    <Route render={() => <h1>Not found</h1>}/>
+                    {/* <Redirect from="/" to="/posts" /> */}
+                </Switch>
             </div>
         );
     }
